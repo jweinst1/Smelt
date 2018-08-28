af042f92b83824f8556a395cfcd7a89d8ea200e0
Josh Weinstein <jweinst1@berkeley.edu>
2018-08-27 17:26:17 -0700

    Create Smelt.h

---
+++ include/Smelt.h
@@ -0,0 +1,10 @@
+#ifndef LIB_SMELT_H
+#define LIB_SMELT_H
+/*Main Smelt Library Header for static linkage*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#endif // LIB_SMELT_H
